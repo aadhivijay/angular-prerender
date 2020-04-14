14925b1056e777769295a9559c169219c048d2ae
ravjduker <aadhivijay@examly.in>
2020-04-14 20:26:32 +0530

    completed

---
+++ src/app/components/side-menu/side-menu.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { SideMenuComponent } from './side-menu.component';
+
+@NgModule({
+    declarations: [SideMenuComponent],
+    exports: [SideMenuComponent]
+})
+export class SideMenuModule {
+
+}
